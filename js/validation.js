966f5b199c81b3ebce433ab335c9bb8941880fb4
Emmanuel Orubele <blontf@gmail.com>
2020-07-10 16:36:22 -0400

    Changes

---
+++ js/validation.js
@@ -0,0 +1,19 @@
+// get element 
+
+var email = document.getElementsById('exampleInputEmail1');
+var password = document.getElementById('exampleInputPasswords');
+var form = document.getElementById('form');
+var errorValue = document.getElementById('error')
+
+
+form.addEventListener('submit', (e) => {
+    let errorMessages = []
+    if (email.value === '' || name.value == null) {
+        errorMessages.push("Email is required")
+    }
+    if (errorMessages.length > 0) {
+        e.preventDefault()
+        errorValue.innerText = errorMessages.join(', ')
+    }
+
+})
