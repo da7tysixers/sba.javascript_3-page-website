1bda81bb38ab078b3fd208d057b0414e5d2fad4b
Emmanuel Orubele <blontf@gmail.com>
2020-07-10 16:44:40 -0400

    Changes to my git files

--- js/validation.js
+++ js/validation.js
@@ -11,6 +11,14 @@ form.addEventListener('submit', (e) => {
     if (email.value === '' || name.value == null) {
         errorMessages.push("Email is required")
     }
+    if (password.length <= 6) {
+        errorMessages.push('Passsword must be longer than six characters');
+    }
+
+    if (password === 'password') {
+        errorMessages.push('Passsword must not be password');
+    }
+
     if (errorMessages.length > 0) {
         e.preventDefault()
         errorValue.innerText = errorMessages.join(', ')
